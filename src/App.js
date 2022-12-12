52ec6a631f5f2dd4c1d3545948ad5ed9c3300217
JUNHO <junhop0708@gmail.com>
2022-12-12 21:38:04 +0900

    to do list

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-/* styles라는 javascript Object를 import한 다음, className props에
-styles의 title안에 적용한 css selector이름을 적어서 호출 */
-function Hello() {
-
-  /* case 1. 함수 실행 후에 리턴 함수를 호출 */
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-
-  /* case 2. 함수 실행 후에 리턴 함수를 호출 */
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  /* 결론적으로 hello() 함수에서 return 해주는 결과값은 <h1> 태그안에 있는 Hello text임 */
-  return <h1>Hello</h1>;
-}
-
-/* App()함수에서 Hello component를 선언해서 사용 */
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  /* useState에 선언한 modifying 함수인 setToDo()함수 선언 및 실행 */
+  const onChange = (event) => setToDo(event.target.value);
+
+  /* submit 용도로 사용하기 위한 함수 생성 */
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+
+    /* useState에 선언한 modifying 함수인 setToDos() 함수 선언 및 실행 
+    useState의 값은 배열로 지정 */
+    //setToDos((currentArray) => [toDo, ...currentArray]); /* 변수를 수정하기 위해서는 수정 함수를 사용 */
+    setToDos(function(currentArray){
+      return [toDo, ...currentArray] // ...currentArray (...Object : array안에 element를 return)
+    });
+    
+    setToDo(""); /* 변수를 수정하기 위해서는 수정 함수를 사용 */
+  };
+
+  console.log(toDos);
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
+
 export default App;
